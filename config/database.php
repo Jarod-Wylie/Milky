ebca6b2f50a30ccc629542f5e2548f548a4fe824
Your Name <jardo_atmai@yahoo.com>
2019-05-14 15:02:34 -0400

    add heroku database

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_efa304ec2438da5'),
+            'username' => env('DB_USERNAME', 'b6a891634248e7'),
+            'password' => env('DB_PASSWORD', 'ae18da05'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
